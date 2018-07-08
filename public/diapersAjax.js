38f2bd9f12a6a79b5c1ffee22252d9beb01154d3
Hyrum Gomez <hyrumgomez@gmail.com>
2018-07-07 22:49:42 -0600

    Added code and functionality for the second milestone.

---
+++ public/diapersAjax.js
@@ -0,0 +1,47 @@
+function callAjax(url, callback) {
+	var xmlObj = new XMLHttpRequest();
+	xmlObj.onreadystatechange = function () {
+		if (xmlObj.readyState == 4 && xmlObj.status == 200) {
+			callback(xmlObj.responseText);
+		}
+	}
+	xmlObj.open("GET", url, true);
+	xmlObj.send();
+}
+
+function getLastDiapers() {
+	console.log("Searching...");
+
+	var fname = document.getElementById("fnameView").value;
+	var lname = document.getElementById("lnameView").value;
+
+	console.log("First: ", fname);
+	console.log("Last: ", lname);
+
+	var url = "/getLastDiapers?name=" + fname + " " + lname;
+    console.log("The url before calling ajax is: ", url);
+	callAjax(url, handleResultList)
+}
+
+function handleResultList(results) {
+	console.log("Back from AJAX with result: ");
+	console.log("result");
+  
+    var formatHTML = "";
+    
+    var myObj = JSON.parse(results);
+    console.log("Outside loop, name is: ", myObj[1].name);
+    
+    var x;
+    for (var i = 0; i < myObj.length; i++) {
+			var name = myObj[i].name;
+            var lastChanged = myObj[i].last_changed;
+            var status = myObj[i].status;
+            console.log("Inside for loop name is: ", name);
+            console.log("Inside for loop last changed is: ", lastChanged);
+            console.log("Inside for loop status is: ", status);
+        
+            formatHTML += "Name: " + name + ", Last Changed on " + lastChanged + ", status: " + status + "<br>";
+		}
+    document.getElementById("diaperResult").innerHTML = formatHTML;
+}
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const { Pool } = require('pg');
+
+const connectionString = process.env.DATABASE_URL || "postgres://caregiver:responsible@localhost:5432/nodeproject";
+
+const pool = new Pool({connectionString: connectionString});
+
+app.set("port", (process.env.PORT || 5000))
+  .use(express.json())
+  .use(express.urlencoded( {extended: true} ))
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+//  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('views/index'))
+  .get('/getLastDiapers', getLastDiapers)
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+function getLastDiapers(req, res) {
+
+	var name = req.query.name;
+	console.log("The name inside of server.js getLastDiapers is: " + name);
+	// TODO: We should really check here for a valid id before continuing on...
+
+	// use a helper function to query the DB, and provide a callback for when it's done
+	getLastDiapersFromDb(name, function(error, result) {
+		// This is the callback function that will be called when the DB is done.
+		// The job here is just to send it back.
+
+		// Make sure we got a row with the person, then prepare JSON to send back
+		if (error || result == null) {
+			res.status(500).json({success: false, data: error});
+		} else {
+			var person = result[0];
+			res.status(200).json(result);
+		}
+	});
+}
+
+function getLastDiapersFromDb(name, callback) {
+	console.log("Getting person from DB with name: " + name);
+
+//	var sql = "SELECT * FROM parent WHERE name = $1";
+    var sql = "SELECT p.name, dc.last_changed, ds.status FROM diaper_change dc INNER JOIN parent p ON dc.parent_id=(SELECT id FROM parent WHERE name=$1) INNER JOIN diaper_status ds ON ds.id=dc.status_id WHERE p.name=$1";
+	var params = [name];
+
+	pool.query(sql, params, function(err, result) {
+		// If an error occurred...
+		if (err) {
+			console.log("Error in query: ")
+			console.log(err);
+			callback(err, null);
+		}
+
+		// Log this to the console for debugging purposes.
+		console.log("Found result: " + JSON.stringify(result.rows));
+		// console.log(result.rows);
+
+		// (The first parameter is the error variable, so we will pass null.)
+		callback(null, result.rows);
+	});
+
+} // end of getPersonFrom
